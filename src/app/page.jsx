45d736f484de4158df57ff68fedeb0e8a5f0c910
gnomin200408-blip <gnomin200408@gmail.com>
2026-04-08 12:25:25 +0800

    duusav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
     date: "",
     image: "",
   });
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
 
   const steps = [Stepone, Steptwo, Stepthree, Stepfour];
   const StepForm = steps[step];
